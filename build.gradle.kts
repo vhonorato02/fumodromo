d1070dd788fe405c3e134327a7332c4e564beb31
José Victor Honorato <33707633+vhonorato02@users.noreply.github.com>
2026-02-28 03:23:48 -0300

    Revamp UI and modernize Android/Kotlin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+    id("com.android.application") version "9.0.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+    id("com.google.devtools.ksp") version "2.3.5" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
 }
